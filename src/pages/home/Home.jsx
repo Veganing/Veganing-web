4ea26314b1186a57d4280791944da0a94a76fa77
Baeksangjun <juny200@naver.com>
2025-11-22 15:27:40 +0900

    비건의 혜텍 부분 완료

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/HeroSection";
+import { VeganBenefits } from "./components/VeganBenefits";
 import WhatIsVegan from "./components/WhatIsVegan";
 function Home() {
     return (
         <>
             <HeroSection />
             <WhatIsVegan />
+            <VeganBenefits />
         </>
     );
 }
